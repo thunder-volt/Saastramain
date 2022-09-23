be53a0a0c8a280b4ae962b9a99012ea75adecd19
thunder-volt <ishu114407@gmail.com>
2022-09-23 17:22:28 +0530

    Profile page completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Profile from './components/Profile';
+import ProfileEdit from './components/ProfileEdit'
+import "./components/profile.css"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Profile /> */}
+      < ProfileEdit />
     </div>
   );
 }
