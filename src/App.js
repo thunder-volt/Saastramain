ee235add88980e12faee7b3fbbe81a13f6c7665c
krithikaa22 <kuruthiga22@gmail.com>
2022-12-12 16:34:08 +0530

    merged e&w

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './App.css';
 import Spons from "./components/Spons";
 import "./styles/styles.css"
@@ -8,34 +7,6 @@ function App() {
     <div className="App">
       <Spons />
     </div>
-=======
-import logo from "./logo.svg";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-import cardData from "./components/cardData";
-import CardComponent from "./components/Card";
-
-function App() {
-  let cardElements = cardData.map((data) => {
-    return <CardComponent data={data} key={data.id} />;
-  });
-
-  return (
-    <>
-    <div className="workshops-landing">
-      <h1 className="title">WORKSHOPS</h1>
-      
-      <div>
-        <button className="explore" onclick="explore()">Explore Workshops </button>{" "}
-      </div>
-      </div>
-      <div className="glassmorphic2">
-        <div className="wrapper">
-        {cardElements}
-        </div>
-      </div>
-    </>
->>>>>>> workshops
   );
 }
 
