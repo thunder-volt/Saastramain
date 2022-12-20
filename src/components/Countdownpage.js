0a53c8d6463e5042a222ce38d86230307968b0e6
krithikaa22 <kuruthiga22@gmail.com>
2022-12-20 17:50:10 +0530

    fixes

--- src/components/Countdownpage.js
+++ src/components/Countdownpage.js
@@ -12,27 +12,16 @@ const Countdown = ({pos}) => {
    
 
     return (
-        
-      
-      
-      
-
-          
-
-          
-
           <div className="theme-body" style={ {
         
             transform: 'translateZ('+pos+'px)',
             
             }}>            
-                    <img src={imgabout} alt="" className='about-img' />
-                    
+                    {/* <img src={imgabout} alt="" className='about-img' /> */}
                     <div className='countdown'>
-                        
-                             { /*<script src="css/uikit.min.css"></script>
+                             <script src="css/uikit.min.css"></script>
                               <script src="js/uikit.min.js"></script>
-                              <script src="js/uikit-icons.min.js"></script>*/}        
+                              <script src="js/uikit-icons.min.js"></script>        
                                       <div class="uk-grid-small uk-child-width-auto" uk-grid uk-countdown="date: 2023-01-27T16:11:57+00:00">
                               <div className='flexcount'>
                                   <div class="uk-countdown-number uk-countdown-days"></div>
@@ -47,13 +36,12 @@ const Countdown = ({pos}) => {
                                   <div class="uk-countdown-number uk-countdown-seconds"></div>
                                   <div class="uk-countdown-label uk-margin-small uk-text-center uk-visible@s">&nbsp; Seconds</div>
                               </div>
-                              
+                              </div>
                               
                           </div>
                     </div>
                     
-           
-            </div>
+     
             
     )
 }
