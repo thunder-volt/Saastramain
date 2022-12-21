907a926714464fac6ce4b91eceb0f1d3ee8c29b6
Jyotsna574 <100153985+Jyotsna574@users.noreply.github.com>
2022-12-21 12:44:49 +0530

    speakers

--- src/components/Speakers.js
+++ src/components/Speakers.js
@@ -12,58 +12,73 @@ const Speakers = ({pos}) => {
 
                 <div className='speakers-slider'>
                     <span style={{ "--i": 1 }}>
-                        <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt="" />
+                        <img src="https://media.news9live.com/h-upload/2022/07/11/465349-rnravitngovernor.webp"alt="" />
                         <div className="speakers-content">
                             <div className='speakers-info'>
-                                <h2>Speakers Name</h2>
+                            <div className="overlay">
+                                <h2>Thiru R. N.Ravi</h2>
                                 <h3>Jounalist</h3>
-                                <h3>Date of Lecture: 22/01/2023</h3>
+                                <h3>Date of Lecture: 22/01/2023</h3></div>
                             </div>
                         </div>
 
                     </span>
-                    <span style={{ "--i": 2 }}><img src="https://www.whatsappimages.in/wp-content/uploads/2021/07/Top-HD-sad-quotes-for-whatsapp-status-in-hindi-Pics-Images-Download-Free.gif" alt="" />
+                    <span style={{ "--i": 2 }}><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Dr_M._S._Valiathan.jpeg/220px-Dr_M._S._Valiathan.jpeg" alt="" />
                         <div className="speakers-content">
                             <div className='speakers-info'>
-                                <h2>Speakers Name</h2>
+                            <div className="overlay">
+                                <h2>Dr. M.S. Valiathan</h2>
                                 <h3>Jounalist</h3>
-                                <h3>Date of Lecture: 22/01/2023</h3>
+                                <h3>Date of Lecture: 22/01/2023</h3></div>
                             </div>
                         </div>
                     </span>
-                    <span style={{ "--i": 3 }}><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgdZpEDslKnjBmPC6dlF6pf9Q2m1o5aMdHwg&usqp=CAU" alt="" />
+                    <span style={{ "--i": 3 }}><img src="https://www.pragyan.org/20/home/gl_and_crossfire/rajagopala_chidambaram/rajagopala_chidambaram.jpg" alt="" />
                         <div className="speakers-content">
                             <div className='speakers-info'>
-                                <h2>Speakers Name</h2>
+                                <div className="overlay">
+                                <h2>Dr. Rajagopala Chidambaram</h2>
                                 <h3>Jounalist</h3>
-                                <h3>Date of Lecture: 22/01/2023</h3>
+                                <h3>Date of Lecture: 22/01/2023</h3></div>
                             </div>
                         </div>
                     </span>
-                    <span style={{ "--i": 4 }}><img src="https://static.vecteezy.com/packs/media/vectors/term-bg-1-3d6355ab.jpg" alt="" />
+                    <span style={{ "--i": 4 }}><img src="https://nettv4u.com/imagine/23-05-2017/srinivas-mohan.png" alt="" />
                         <div className="speakers-content">
                             <div className='speakers-info'>
-                                <h2>Speakers Name</h2>
+                                <div className="overlay">
+                                <h2>Mr. Srinivas Mohan</h2>
                                 <h3>Jounalist</h3>
-                                <h3>Date of Lecture: 22/01/2023</h3>
+                                <h3>Date of Lecture: 22/01/2023</h3></div>
                             </div>
                         </div>
                     </span>
-                    <span style={{ "--i": 5 }}><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPdbKdjTNV07Ld2FDo_m37tAYQjNis8VrGVjS8wxHhklcINZXFPybIvNNov-VwYMjzPdg&usqp=CAU" alt="" />
+                    <span style={{ "--i": 5 }}><img src="https://www.indianetzone.com/photos_gallery/65/12_Sivathanu_Pillai.jpg" alt="" />
                         <div className="speakers-content">
                             <div className='speakers-info'>
-                                <h2>Speakers Name</h2>
+                            <div className="overlay">
+                                <h2>Dr. A. Sivathanu Pillai</h2>
                                 <h3>Jounalist</h3>
-                                <h3>Date of Lecture: 22/01/2023</h3>
+                                <h3>Date of Lecture: 22/01/2023</h3></div>
                             </div>
                         </div>
                     </span>
-                    <span style={{ "--i": 6 }}><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAd2XziS7czWcS1oK9KMY_R4FAGX4l6ZFdrA&usqp=CAU" alt="" />
+                    <span style={{ "--i": 6 }}><img src="https://upload.wikimedia.org/wikipedia/commons/7/7e/Montekahuwalia.jpg" alt="" />
                         <div className="speakers-content">
                             <div className='speakers-info'>
-                                <h2>Speakers Name</h2>
+                            <div className="overlay">
+                                <h2>Dr. Montek Singh Ahluwalia</h2>
                                 <h3>Jounalist</h3>
-                                <h3>Date of Lecture: 22/01/2023</h3>
+                                <h3>Date of Lecture: 22/01/2023</h3></div>
+                            </div>
+                        </div></span>
+                        <span style={{ "--i": 7 }}><img src="https://pbs.twimg.com/profile_images/1473706332104663048/mYM4x-7Q_400x400.jpg" alt="" />
+                        <div className="speakers-content">
+                            <div className='speakers-info'>
+                            <div className="overlay">
+                                <h2>Mrs. Vineeta Singh</h2>
+                                <h3>Jounalist</h3>
+                                <h3>Date of Lecture: 22/01/2023</h3></div>
                             </div>
                         </div></span>
                 </div>
