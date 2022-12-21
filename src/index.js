42cc4573c1dd2c0707b27e428c23e1e34eeba6dd
krithikaa22 <kuruthiga22@gmail.com>
2022-12-21 19:03:41 +0530

    api url

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import {
 } from "@apollo/client";
 
 const link = new HttpLink({
-  uri: "http://localhost:8000/graphql",
+  uri: "https://api.events.shaastra.org",
   credentials: "include",
 });
 
