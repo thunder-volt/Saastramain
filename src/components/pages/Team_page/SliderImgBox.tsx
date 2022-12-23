bb119a84a06b1d97e167e3cc8ba68680111224fc
krithikaa22 <kuruthiga22@gmail.com>
2022-12-23 09:52:12 +0530

    team

--- src/components/pages/Team_page/SliderImgBox.tsx
+++ src/components/pages/Team_page/SliderImgBox.tsx
@@ -26,15 +26,14 @@ interface Props {
 }
 
 const SliderImgBox = (props: Props) => {
-  console.log(props)
   return (
     <div className="sliderImgBox">
       <div className="profileCard">
         <div className="profileCard_header">
           <div>
-            <h2>{props.profile?.name}</h2>
-            <p>{props.profile?.role}</p>
-            <p>{props.profile?.dept}</p>
+            <h2>{props.profile.name}</h2>
+            <p>{props.profile.role}</p>
+            <p>{props.profile.dept}</p>
           </div>
         </div>
           {/* <Flex
@@ -59,7 +58,7 @@ const SliderImgBox = (props: Props) => {
         {/*<div className="profileDescription">{props.profile.desc}</div>*/}
       </div>
       <div className="imgCover">
-        <img src={props.profile?.imgURL} alt={props.profile?.imgURL} />
+        <img src={props.profile.imgURL} alt={props.profile.imgURL} />
       </div>
     </div>
   );
