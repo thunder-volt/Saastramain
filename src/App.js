b04b468f9b9971fecf9fc26fdda0065b353958fb
arnav <goelarnav12@gmail.com>
2023-01-05 14:34:24 +0530

    firdt commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ChessBlitz from './blitz-chess.js';
+
+function App() {
+  return (
+    <div className="App">
+      <ChessBlitz />
+    </div>
+  );
+}
+
+export default App;
