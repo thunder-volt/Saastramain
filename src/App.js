e47c98543a2933351bfb436b6861911d2083ee32
MentalLink <63054696+MentalLink@users.noreply.github.com>
2023-01-05 18:20:54 +0530

    Revert "Update App.js"
    This reverts commit 659fe3530b6f2c32fae533f1a0156e29a34bc2bf.

--- src/App.js
+++ src/App.js
@@ -32,49 +32,9 @@ import Temp from "./Components/tempHome";
 
 import Contact from "./Contact";
 
-
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import { Modal2 } from './Components/Modal2';
-
-
-const Container = styled.div`
-display: flex;
-justify-content: center;
-  align-items: center;
-  height: 10vh;
-  float: right;
-`;
-
-const ButtonModal = styled.button`
-  position: absolute;
-  right:30px;
-  bottom:30px;
-  min-width: 75px;
-  padding: 10px 25px;
-  border-radius: 4px;
-  border: none;
-  background: linear-gradient(224.52deg, #0B0454 1.63%, #7E10B3 70.6%, rgba(130, 16, 176, 0.88) 90.31%, rgba(162, 17, 156, 0.82) 101.2%);;
-  color: #fff;
-  font-size: 24px;
-  cursor: pointer;  
-  transform: rotate(0deg);
-`;
-
 function App() {
-  const [showModal2, setShowModal2] = useState(false);
-
-  const openModal2 = () => {
-    setShowModal2(prev => !prev);
-  };
   return (
     <div>
-    
-      <ButtonModal onClick={openModal2}>SALES!</ButtonModal>
-      <Container>
-        <Modal2 showModal2={showModal2} setShowModal2={setShowModal2} />
-      </Container>
-
       <Router>
         <Routes>
           <Route exact path="/" element={<Homepage />} />
